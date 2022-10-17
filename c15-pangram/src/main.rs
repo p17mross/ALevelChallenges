12c43589960e4b3225eae17526392eb2fcf516ef
Mark Ross <markross470@gmail.com>
2022-10-17 18:14:55 +0100

    Added comments to c15-pangram

--- c15-pangram/src/main.rs
+++ c15-pangram/src/main.rs
@@ -1,8 +1,13 @@
 use std::io::{self, BufRead, Write};
 
+// Every letter to require
 const ALPHABET: &str = "abcdefghijklmnopqrstuvwxyz";
+
+/// Function to check if a string is a pangram
 fn is_pangram(s: &str) -> bool {
+    // Get lowercase of string
     let lower = s.to_lowercase();
+    // Check all letters
     for c in ALPHABET.chars() {
         if !lower.contains(c) {return false}
     }
@@ -10,11 +15,14 @@ fn is_pangram(s: &str) -> bool {
 }
 
 fn main() {
+    // Input string
     let stdin = io::stdin();
     let mut s: String = "".to_string();
     print!("Enter a string: ");
+    // Flush stdout
     io::stdout().flush().unwrap();
     stdin.lock().read_line(&mut s).unwrap();
+    // Remove newline from end of string
     s = s[0..s.len() - 1].to_string();
     if is_pangram(&s) {
         println!("'{s}' is a pangram");
