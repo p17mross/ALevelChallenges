44335cdc8cd1570bc6b8a5c991939acba4897997
Mark Ross <markross470@gmail.com>
2022-10-26 18:31:36 +0100

    Added readme to c01-factorial

--- c01-factorial/README.md
+++ c01-factorial/README.md
@@ -1,2 +1,5 @@
-# Test
-to check if readmes in subfolders are shown
+# Challenge 1 - Factorial
+
+Two functions with different implementations of computing a factorial
+
+![image](images/example.png)
--- c01-factorial/src/main.rs
+++ c01-factorial/src/main.rs
@@ -1,19 +1,29 @@
+/// A function to calculate the factorial of a number using a loop
 fn factorial_loop(n: u32) -> u32 {
+    // Set starting value
     let mut factorial: u32 = 1;
+    // For every i 1 < i < n, multiply value by i
     for i in 1..n + 1 {
         factorial *= i;
     }
+    // Return value
     factorial
 }
 
+/// A function to calculate the factorial of a number recursively
 fn factorial_recursive(n: u32) -> u32 {
     match n {
+        // Base case: factorial of 0 is 1
         0 => 1,
+        // Recursive case: factorial of n is n times the factorial of n - 1
         _ => factorial_recursive(n - 1) * n
     }
 }
 
 fn main() {
-    println!("{}", factorial_loop(10));
-    println!("{}", factorial_recursive(10));
+    println!("Factorial of 5 computed with a loop is {}", factorial_loop(5));
+    println!("Factorial of 5 computed recursively is {}", factorial_recursive(5));
+
+    println!("Factorial of 10 computed with a loop is {}", factorial_loop(10));
+    println!("Factorial of 10 computed recursively is {}", factorial_recursive(10));
 }
